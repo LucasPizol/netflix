160f056c9cf1b655b0504138997d50b71b9341fa
Lucas Pizol Ferreira <131599316+LucasPizol@users.noreply.github.com>
2023-10-16 17:35:30 -0300

    Criação do header da página inicial sem autenticação

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import Head from "next/head";
-import styles from "../styles/HomeNoAuth.module.scss"
+import styles from "../styles/HomeNoAuth.module.scss";
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>LucasFlix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="LucasFlix" key="title" />
+        <meta name="description" content="Tenha acesso aos melhores conteúdos de programação."/>
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth/>
+      </main>
     </>
   );
 };
---
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { Container, Button } from "reactstrap";
+
+const HeaderNoAuth = () => {
+  return (
+    <>
+      <div className={styles.ctaSection}>
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+        <p>Se cadastre para ter acesso aos cursos</p>
+        <img
+          src="/homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+      </div>
+      <Container className={styles.nav}>
+        <img
+          src="/logoOnebitflix.svg"
+          alt="LogoOnebitflix"
+          className={styles.imgLogoNav}
+        />
+        <div>
+          <Link href="/login">
+            <Button className={styles.navBtn} outline>
+              Entrar
+            </Button>
+          </Link>
+
+          <Link href="/register">
+            <Button className={styles.navBtn} outline>
+              Quero fazer parte
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default HeaderNoAuth;
