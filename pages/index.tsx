d923930a9c752e8648606e554f8e8426de747476
Lucas Pizol Ferreira <131599316+LucasPizol@users.noreply.github.com>
2023-10-17 09:04:12 -0300

    Criação da apresentação da págian sem autenticação + ajuste de responsividade

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
 import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
+import PresentationSection from "@/src/components/homeNoAuth/presentationSection";
+import CardSection from "@/src/components/homeNoAuth/cardSection";
 
 const HomeNoAuth = () => {
   return (
@@ -9,10 +11,17 @@ const HomeNoAuth = () => {
         <title>LucasFlix</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
         <meta property="og:title" content="LucasFlix" key="title" />
-        <meta name="description" content="Tenha acesso aos melhores conteúdos de programação."/>
+        <meta
+          name="description"
+          content="Tenha acesso aos melhores conteúdos de programação."
+        />
       </Head>
       <main>
-        <HeaderNoAuth/>
+        <div className={styles.sectionBackground}>
+          <HeaderNoAuth />
+          <PresentationSection />
+        </div>
+        <CardSection/>
       </main>
     </>
   );
---
+++ src/components/homeNoAuth/cardSection/index.tsx
@@ -0,0 +1,20 @@
+import { Container } from "reactstrap"
+import styles from "./styles.module.scss"
+
+const CardSection = () => {
+  return (
+    <>
+      <p>O QUE VOCÊ VAI ACESSAR</p>
+      <Container>
+        <div>
+            <p>FRONT-END</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, consectetur neque, corrupti laboriosam inventore cumque maiores rem soluta est fuga cum? Alias perferendis totam rem ipsam ad hic magni explicabo.
+            </p>
+        </div>
+      </Container>
+    </>
+  )
+}
+
+export default CardSection
