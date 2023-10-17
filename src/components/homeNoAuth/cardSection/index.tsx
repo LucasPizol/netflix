973f9a7bfad81d4e954fda236dc10df1a9dc124e
Lucas Pizol Ferreira <131599316+LucasPizol@users.noreply.github.com>
2023-10-17 09:18:31 -0300

    Criação dos cards

--- src/components/homeNoAuth/cardSection/index.tsx
+++ src/components/homeNoAuth/cardSection/index.tsx
@@ -1,20 +1,68 @@
-import { Container } from "reactstrap"
-import styles from "./styles.module.scss"
+import { Container } from "reactstrap";
+import styles from "./styles.module.scss";
 
 const CardSection = () => {
   return (
     <>
-      <p>O QUE VOCÊ VAI ACESSAR</p>
-      <Container>
-        <div>
-            <p>FRONT-END</p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, consectetur neque, corrupti laboriosam inventore cumque maiores rem soluta est fuga cum? Alias perferendis totam rem ipsam ad hic magni explicabo.
-            </p>
+      <p className={styles.sectionTitle}>O QUE VOCÊ VAI ACESSAR</p>
+      <Container className="d-flex flex-wrap justify-content-center gap-4 pb-5">
+        <div className={styles.card1}>
+          <p className={styles.cardTitle}>FRONT-END</p>
+          <p className={styles.cardDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+            consectetur neque, corrupti laboriosam inventore cumque maiores rem
+            soluta est fuga cum? Alias perferendis totam rem ipsam ad hic magni
+            explicabo.
+          </p>
+        </div>
+        <div className={styles.card2}>
+          <p className={styles.cardTitle}>BACK-END</p>
+          <p className={styles.cardDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+            consectetur neque, corrupti laboriosam inventore cumque maiores rem
+            soluta est fuga cum? Alias perferendis totam rem ipsam ad hic magni
+            explicabo.
+          </p>
+        </div>
+        <div className={styles.card3}>
+          <p className={styles.cardTitle}>MOBILE</p>
+          <p className={styles.cardDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+            consectetur neque, corrupti laboriosam inventore cumque maiores rem
+            soluta est fuga cum? Alias perferendis totam rem ipsam ad hic magni
+            explicabo.
+          </p>
+        </div>
+        <div className={styles.card4}>
+          <p className={styles.cardTitle}>GIT E GITHUB</p>
+          <p className={styles.cardDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+            consectetur neque, corrupti laboriosam inventore cumque maiores rem
+            soluta est fuga cum? Alias perferendis totam rem ipsam ad hic magni
+            explicabo.
+          </p>
+        </div>
+        <div className={styles.card5}>
+          <p className={styles.cardTitle}>PROJETOS</p>
+          <p className={styles.cardDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+            consectetur neque, corrupti laboriosam inventore cumque maiores rem
+            soluta est fuga cum? Alias perferendis totam rem ipsam ad hic magni
+            explicabo.
+          </p>
+        </div>
+        <div className={styles.card6}>
+          <p className={styles.cardTitle}>CARREIRA</p>
+          <p className={styles.cardDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+            consectetur neque, corrupti laboriosam inventore cumque maiores rem
+            soluta est fuga cum? Alias perferendis totam rem ipsam ad hic magni
+            explicabo.
+          </p>
         </div>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default CardSection
+export default CardSection;
